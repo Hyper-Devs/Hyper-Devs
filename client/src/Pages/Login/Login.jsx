ba4ba4db57b0ccdc0545d12c4e766dd247a7d092
Emmanuel Zhade Lachica <eblachica@up.edu.ph>
2023-03-03 15:16:16 +0800

    added images to assets and updated login page

--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 
 function Login() {
   return (
-    <div className='bg'>
-      <div className='container'>
-        <h1>Gate-Access System</h1>
+    <div className='backgroundImage'>
+    <form>
+      <div className='formContainer'>
+        <h3>UNIVERSITY OF THE PHILIPPINES CEBU HIGH SCHOOL</h3>
+        <div className='formInner'>
+          <label>ID Number</label>
+          <input type="text" name='idNumber' id='idNumber'/>
+          <label>Password:</label>
+          <input type="password" name="password" id="password" />
+          <button>Login</button>
+          </div>
       </div>
+    </form>
     </div>
+
   );
 }
 
