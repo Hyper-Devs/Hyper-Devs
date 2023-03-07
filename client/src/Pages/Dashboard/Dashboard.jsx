2aa2d84ca553a8acd9cb2e68d50e80e6c4953dfa
gebmecod <jybravo@up.edu.ph>
2023-03-08 00:29:29 +0800

    Merged sidebar to dashboard and added icons

--- client/src/Pages/Dashboard/Dashboard.jsx
+++ client/src/Pages/Dashboard/Dashboard.jsx
@@ -4,11 +4,13 @@ import Greeting from '../../Components/Greeting';
 import Box from '../../Components/Dashboard/Box';
 import DashFoot from '../../Components/Dashboard/Dashboard-Footer';
 import './Dashboard.css'
+import Sidebar from '../../Components/Sidebar/Sidebar'
 
 function Dashboard() {
   return (
     <div className='Dashboard'>
       <Header/>
+      <Sidebar />
       <Greeting />
       <div className='dashboard-title'>Dashboard</div>
       <Box/>
