f6d1b6d911c54c0423f3d80e957ccf9f746cb0c2
gebmecod <84118457+gebmecod@users.noreply.github.com>
2023-03-07 23:39:27 +0800

    Revert "Added sidebar in the dashboard; Added the pages indicated by the sidebar options"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,10 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Login from './Pages/Login/Login';
 import Dashboard from './Pages/Dashboard/Dashboard';
-import Home from './Pages/Home/Home';
-import Database from './Pages/Database/Database';
-import Enroll from './Pages/Enroll/Enroll';
-import Notification from './Pages/Notification/Notification';
-import Override from './Pages/Override/Override';
 
 function App() {
   return (
@@ -23,26 +18,6 @@ function App() {
           <Route
             path='/dashboard'
             element={<Dashboard/>}
-          />
-          <Route
-            path='/home'
-            element={<Home/>}
-          />
-          <Route
-            path='/database'
-            element={<Database/>}
-          />
-          <Route
-            path='/enroll'
-            element={<Enroll/>}
-          />
-          <Route
-            path='/notification'
-            element={<Notification/>}
-          />
-          <Route
-            path='/override'
-            element={<Override/>}
           />
         </Routes>
       </BrowserRouter>
