9695fed23c52efda3e0206cba0aff157f7652a09
Emmanuel Zhade Lachica <eblachica@up.edu.ph>
2023-03-15 00:39:55 +0800

    fixed login page

--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -1,30 +1,26 @@
 import React, { useState } from "react";
 import "./Login.css";
-/*import Footer from "../../Components/footer";*/
+import Footer from "../../Components/footer";
 
 function Login() {
   return (
     <div className="login-container">
-      <div className="form-container">
-        <div className="upcLogo"></div>
-        <form className="loginForm">
-          <div className="inputField">
-            <label>ID Number</label>
-            <input type="text" />
-          </div>
-          <div className="inputField">
-            <label>Password</label>
-            <input type="password" />
-          </div>
-          <button>Login</button>
-          <a href="#">Forgot Password?</a>
-        </form>
-      </div>
       <div className="title">
         <h2>University of the Philippines Cebu High School</h2>
         <h1>Gate Access Notification System</h1>
       </div>
-      {/*<Footer />*/}
+      <div className="form-container">
+        <div className="upcLogo"></div>
+        <form className="loginForm">
+          <label>ID Number</label>
+          <input type="text" />
+          <label>Password</label>
+          <input type="password" />
+          <button>Login</button>
+          <a href="#">Forgot Password?</a>
+        </form>
+      </div>
+      <Footer />
     </div>
   );
 }
