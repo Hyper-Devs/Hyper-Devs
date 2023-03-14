cd8cba0f70b739e6c843156297489b1f62672b65
Emmanuel Zhade Lachica <eblachica@up.edu.ph>
2023-03-15 00:46:03 +0800

    fixed login page

--- client/src/Components/footer.jsx
+++ client/src/Components/footer.jsx
@@ -1,78 +1,79 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
 
-import UPClogo from '../Assets/upc_logo.png'
-import DCSlogo from '../Assets/dept_compsci.png'
-import HDlogo from '../Assets/hd logo black.png'
-
+import UPClogo from "../Assets/upc_logo.png";
+import DCSlogo from "../Assets/dept_compsci.png";
+import HDlogo from "../Assets/hd logo black.png";
 
 function footer() {
   return (
-    <div className='page-footer'>
-      <div className = 'footer-container'>
-        </div>
-      <div className='footer-content'>
-        <div className='presented-to-section'>
-          <div className='presented-to-stmnt'>
+    <div className="page-footer">
+      <div className="footer-container"></div>
+      <div className="footer-content">
+        <div className="presented-to-section">
+          <div className="presented-to-stmnt">
             <p>Presented to:</p>
           </div>
-          <div className='recipient1'>
+          <div className="recipient1">
             <p>Department of Computer Science</p>
           </div>
-          <div className='recipient2'>
+          <div className="recipient2">
             <p>University of the Philippines Cebu</p>
           </div>
           <div>
-            <img className='DCS-logo'> src={DCSlogo} alt="DepSciLogo"</img>
+            <img className="DCS-logo"> src={DCSlogo} alt="DepSciLogo"</img>
           </div>
           <div>
-            <img className='UPC-logo'> src={UPClogo} alt="UnivPhilCLogo"</img>
+            <img className="UPC-logo"> src={UPClogo} alt="UnivPhilCLogo"</img>
           </div>
         </div>
 
-        <div className='collab-section'>
-          <div className='collab-stmnt'>
+        <div className="collab-section">
+          <div className="collab-stmnt">
             <p>In collaboration with:</p>
           </div>
-          <div className='collaborator'>
+          <div className="collaborator">
             <p>University of the Philippines Cebu High School</p>
           </div>
         </div>
 
-        <div className='copyright-section'>
-          <div className='copyright-stmnt'>
+        <div className="copyright-section">
+          <div className="copyright-stmnt">
             <p>ⓒUPCHS 2023. All rights reserved.</p>
           </div>
         </div>
 
-        <div className='dev-section'>
-          <div className='dev-stmnt'>
+        <div className="dev-section">
+          <div className="dev-stmnt">
             <p>Designed and implemented by:</p>
           </div>
-          <div className='developers'>
+          <div className="developers">
             <p>Hyper Devs</p>
           </div>
           <div>
-            <img className='hyperdevs-logo'> src={HDlogo} alt="hyperDevsLogo"</img>
-            </div>
+            <img className="hyperdevs-logo">
+              {" "}
+              src={HDlogo} alt="hyperDevsLogo"
+            </img>
+          </div>
         </div>
 
-        <div className='reach-us-section'>
-          <div className='reach-us-stmnt'>
+        <div className="reach-us-section">
+          <div className="reach-us-stmnt">
             <p>Reach us:</p>
           </div>
-          <div className='email-address'>
+          <div className="email-address">
             <p>upc.hyperdevs@gmail.com</p>
           </div>
         </div>
-
-      </div>  
+      </div>
     </div>
-  )
+  );
 }
 
-export default footer
-    {/*<div className='page-footer'>
+export default footer;
+{
+  /*<div className='page-footer'>
       <div className='footer-content-1'>
         <p>Design and Implemented by:</p>
       </div>
@@ -81,4 +82,5 @@ export default footer
       </div>
       <div>
         <img className='footer-hdlogo' src={HDLogo} alt="hyperDevsLogo" />
-  </div>*/}
+  </div>*/
+}
