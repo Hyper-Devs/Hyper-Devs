79a4bbeed025f136f9b93947b733ae9abf5d74dc
JohnM <jmclaquinta327@gmail.com>
2023-03-15 15:04:32 +0800

    ...

--- client/src/Components/Greeting.jsx
+++ client/src/Components/Greeting.jsx
@@ -5,9 +5,7 @@ import border from "../Assets/admin_border.png"
 function Greeting({ name = "Noreen"}) {
     return (
         <div className='Greeting'>
-            <img src={border}/>
-            <div className='welcome-message'><p>Welcome back, {name}.</p><p>Logout</p></div>
-            <img src={border}/>
+            <div className='welcome-message'><p>Override</p></div>
             
         </div>
     )
