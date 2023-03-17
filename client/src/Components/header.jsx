a45f7fca7096de10d1de7f26b81d3819eacf845e
Myra Daitol <mybaymax21@gmail.com>
2023-03-17 15:49:52 +0800

    fixed footer container error, updated header

--- client/src/Components/header.jsx
+++ client/src/Components/header.jsx
@@ -8,9 +8,6 @@ function Header() {
             <p className='Header-text'>
                 Gate Access Notification System
             </p>
-            <p className='program-functions'>
-            Access • Track • Notify • Alert • Override
-            </p>
             <p className='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
     )
