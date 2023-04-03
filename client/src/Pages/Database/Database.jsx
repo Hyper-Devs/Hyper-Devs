f441de0d10cf2529dab26b141de5bb52c7410628
Myra Daitol <mybaymax21@gmail.com>
2023-04-03 13:29:10 +0800

    added bootstrap to root

---
+++ client/src/Pages/Database/Database.jsx
@@ -0,0 +1,12 @@
+import "./Database.css";
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery/dist/jquery.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+function Database(){
+    return(
+        <div></div>
+    )
+}
+export default Database
