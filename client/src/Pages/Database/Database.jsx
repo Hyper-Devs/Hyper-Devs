7dc2eb2b727ca7c94a8e89d5148ef36432d22897
Myra Daitol <mybaymax21@gmail.com>
2023-04-05 02:54:52 +0800

    database is fixed and done

--- client/src/Pages/Database/Database.jsx
+++ client/src/Pages/Database/Database.jsx
@@ -5,13 +5,14 @@ import Sidebar from '../../Components/Sidebar/Sidebar'
 import Header from '../../Components/header'
 import DatabaseModifier from "../../Components/Database/Database-Modifier";
 import DatabaseResult from "../../Components/Database/Database-Result";
+import Header2 from "../../Components/Database/Header/Header2";
+import Footer1 from "../../Components/footer";
 
 function Database(){
     
     return(
     <div className="database-container">
-        {/* <div className="database-default">
-        <Header/>
+        <div className="database-default">
         <Sidebar
                 buttonState={{
                     item1: false,
@@ -20,11 +21,15 @@ function Database(){
                     item4: false,
                     item5: false,
                 }} />
-        </div> */}
+        </div>
         
-        <div className="database-content-container">
+        <div className="database-content-container ">
+            
+            <Header2/>
+                <div className="page-title"><p>SYSTEM DATABASE</p></div>
             <DatabaseModifier/>
             <DatabaseResult/>
+            <Footer1/>
         </div> 
     </div>
 
