ab504284c52779c85392b7cc98bb99bbd9d01e30
Myra Daitol <mybaymax21@gmail.com>
2023-04-15 07:53:25 +0800

    added sidebar to Enroll

--- client/src/Pages/Enroll/Enroll.jsx
+++ client/src/Pages/Enroll/Enroll.jsx
@@ -15,26 +15,39 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 function Enroll(){
     return(
-        <div>
-            <div>
+        <div className="enroll-container">
+            <div className="enroll-sidebar">
+            <Sidebar
+                buttonState={{
+                    item1: false,
+                    item2: false,
+                    item3: true,
+                    item4: false,
+                    item5: false,
+                }} />
+            </div>
+            <div className="enroll-default">
                 <Header2/>
-                {/* <EnrollModifier1/>
-                <EnrollIndResult/> */}
-                </div>
-
-                <div class="container-fluid ">
-                    <div class="row">
-                        <div class="col-4">
-                            <EnrollModifier1/>
+                <div className="enroll-content">
+                <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-4">
+                                <EnrollModifier1/>
+                            </div>
+                            <div class="col-8">
+                                {/* <BSNewSResults/> */}
+                                {/* <EnrollBNewSYResults/> */}
+                                <EnrollIndResult/>
+                            </div>
                         </div>
-                        <div class="col-8">
-                            <BSNewSResults/>
-                            {/* <EnrollBNewSYResults/> */}
-                            {/* <EnrollIndResult/> */}
-                        </div>
+                        
                     </div>
                 </div>
                 <Footer1/>
+            </div>
+
+
+                
             </div>
             
 
