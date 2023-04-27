6a01bfa67e6c28d6f204b1ef4a991b2fe2620d21
JohnM <jmclaquinta327@gmail.com>
2023-04-27 16:38:04 +0800

    Fixes in the Override page

--- client/src/Pages/Overriding/Override.jsx
+++ client/src/Pages/Overriding/Override.jsx
@@ -3,28 +3,27 @@ import './Override.css';
 import Sidebar from '../../Components/Sidebar/Sidebar';
 import OvrContainer from '../../Components/Override/OvrContainer';
 import PageTitle from '../../Components/PageTitle';
-import Footer from '../../Components/footer'
+
 
 function Override() {
     return (
       <div className='override-container'>
         <div className="override-content-container">
-        <div>
-        <Sidebar
-          buttonState = {{
-            item1: false,
-            item2: false,
-            item3: false,
-            item4: false,
-            item5: true
-          }}
-        />
-        </div>
-        <div className='override-content' >
-            <PageTitle pageName={"Override"}/>
-            <OvrContainer/>
-            <Footer/>
-        </div>
+          <div>
+            <Sidebar
+              buttonState = {{
+                item1: false,
+                item2: false,
+                item3: false,
+                item4: false,
+                item5: true
+              }}
+            />
+          </div>
+          <div className='override-content' >
+              <PageTitle pageName={"Override"}/>
+              <OvrContainer/>
+          </div>
         </div>
       </div>
     );
