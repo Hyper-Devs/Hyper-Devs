7640e867a53dbef5fcd5f130f6afee6da038e0e9
Kienth Tan <mattkienth@gmail.com>
2023-04-30 23:02:00 +0800

    Added Messaging API (works but needs tweaking)

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import express from "express";
 import cors from "cors";
 import routes from './routes/index.js';
+import twilio from 'twilio';
 
+const accountSID = 'ACce43b4fdda36553a50eda2b9b2715277';
+const authToken = '998814625f7567851b4edf36bcb87dd3';
+const client = new twilio(accountSID, authToken);
 
 const app = express();
 app.use(cors());
@@ -9,8 +13,24 @@ app.use(express.json());
 app.use('', routes);
 
 
+// Twilio API
+app.get('/message', (req, res) => {
+  // console.log("this is working")
+  const { recipient, textMessage } = req.query
+
+  // Send text message
+  client.messages.create({
+    body: textMessage,
+    to: "+63"+recipient,
+    from: '+16072702153'
+  }).then((message) =>  console.log(message.body, "Message sent succesfully!"))
+  .catch(error => console.log(error, "Message failed to send!"));
+})
+
+
+
 app.listen(8800, () => {
-  console.log("Connected to backend.");
+  console.log("Connected to backend using port 8800.");
 });
 
 // module.exports=app;
