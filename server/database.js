250aba7d8d3eab21d5e04606735384d86c007e6b
gebmecod <jybravo@up.edu.ph>
2023-06-02 05:38:25 +0800

    deployed database

--- server/database.js
+++ server/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "gans prototype",
+    host: "gansprototype.clc26y9lpo9k.ap-southeast-1.rds.amazonaws.com",
+    user: "admin",
+    password: "password",
+    database: "gans_prototype",
   });
 
 exports.databaseConnection = db;
