4876634c3f903498e5ffd90711e837ddeb0eb86d
Myra Daitol <mybaymax21@gmail.com>
2023-06-12 11:24:23 +0800

    placed spinner at the center of the page

--- client/src/Components/IsAuthenticated.jsx
+++ client/src/Components/IsAuthenticated.jsx
@@ -6,70 +6,74 @@ import jwt_decode from 'jwt-decode';
 import api from "../api/api"
 
 
-function IsAuthenticated ({children}){
+function IsAuthenticated ({children}) {
 
-    const [titleModal, setTitleModal] = useState('');
-    const [bodyModal, setBodyModal] = useState('');
-    const [showModal, setShowModal] = useState(false); // State to control the visibility of the modal
-    const [isLoading, setIsLoading] = useState(true);
-    const [accessIDExists, setAccessIDExists] = useState(false);
+  const [titleModal, setTitleModal] = useState('');
+  const [bodyModal, setBodyModal] = useState('');
+  const [showModal, setShowModal] = useState(false); // State to control the visibility of the modal
+  const [isLoading, setIsLoading] = useState(true);
+  const [accessIDExists, setAccessIDExists] = useState(false);
 
 
-    const handleCloseModal = () => {
-      setShowModal(false);
-      navigate('/');
-    };
+  const handleCloseModal = () => {
+    setShowModal(false);
+    navigate('/');
+  };
 
-    let navigate = useNavigate();
-    const token = localStorage.getItem("accessToken");
+  let navigate = useNavigate();
+  const token = localStorage.getItem("accessToken");
 
-    useEffect(()=>{
-      if (!token) {
+  useEffect(() => {
+    if (!token) {
       setTitleModal("Unauthorized Access.");
-      setBodyModal("Please Login to access the page!")
-      setShowModal(true)
+      setBodyModal("Please Login to access the page!");
+      setShowModal(true);
     } else {
       const decodedToken = jwt_decode(token);
-            const accessID = decodedToken.AccessID;
-            api.get(`/database/check-access-id/${accessID}`)
-                .then(response => {
-                    setAccessIDExists(response.data.exists);
-                    setIsLoading(false);
-                })
-                .catch(error => {
-                    console.error(error);
-                    setIsLoading(false);
-                });
+      const accessID = decodedToken.AccessID;
+      api.get(`/database/check-access-id/${accessID}`)
+        .then(response => {
+          setAccessIDExists(response.data.exists);
+          setIsLoading(false);
+        })
+        .catch(error => {
+          console.error(error);
+          setIsLoading(false);
+        });
     }
-    },[token])
-    
+  }, [token]);
 
-
-    return(
-      <div>
-        <GlobalModal
-            showModal={showModal}
-            title={titleModal}
-            body={bodyModal}
-            onClose={handleCloseModal}
-        />
-        {isLoading && <p>
-          <div className="d-flex justify-content-center">
-            <div className="spinner-border" role="status">
-              <span className="visually-hidden">Loading...</span>
-            </div>
+  return (
+    <div>
+      <GlobalModal
+        showModal={showModal}
+        title={titleModal}
+        body={bodyModal}
+        onClose={handleCloseModal}
+      />
+      {isLoading && (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+          }}
+        >
+          <div className="spinner-border text-warning" role="status">
+            <span className="visually-hidden">Loading...</span>
           </div>
-          
-          </p>}
-        {!isLoading && accessIDExists && <>{children}</>}
-        {!isLoading && !accessIDExists && (
-          <>
-            <p>Server Error. Refresh Page</p>
-            <button onClick={() => navigate('/')}>Go to Login</button>
-          </>
-            )}
-      </div>
-    )
-  }
+        </div>
+      )}
+      {!isLoading && accessIDExists && <>{children}</>}
+      {!isLoading && !accessIDExists && (
+        <>
+          <p>Server Error. Refresh Page</p>
+          <button onClick={() => navigate('/')}>Go to Login</button>
+        </>
+      )}
+    </div>
+  );
+}
 
 export default IsAuthenticated;
